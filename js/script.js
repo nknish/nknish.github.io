c29acd338385f09644f0f8727998bdd5c898f18b
nknish <noahknish@gmail.com>
2024-06-19 21:13:05 -0400

    Initialize grid with hardcoded values

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const gridContainer = document.getElementById('grid-container');
     const gridSize = 10;
     const gridItems = []
+    const gridValues = [[0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
 
 
     // create grid
-    for (let i =0; i < gridSize * gridSize; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridContainer.appendChild(gridItem);
-        gridItems.push(gridItem);
+    for (let i =0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++ ) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.style.backgroundColor = gridValues[i][j] === 1 ? '#dddddd' : '#223388';
+            gridContainer.appendChild(gridItem);
+            gridItems.push(gridItem);
+        }
     }
 
     // switch colors around
